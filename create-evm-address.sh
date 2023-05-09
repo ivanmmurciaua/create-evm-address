be17c13ab965ae544160e274ef3826d38ec7f474
Iván M.M <solucionesemecuadrado@gmail.com>
2023-05-09 21:00:28 +0200

    Comments and clear screen to get a cleaner result

--- create-evm-address.sh
+++ create-evm-address.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# Vars
+# Generate pair of keys
 key=$(openssl ecparam -name secp256k1 -genkey -noout | openssl ec -text -noout)
+
+# Get the public key
 pub=$(echo "$key" | grep pub -A 5 | tail -n +2 | tr -d '\n[:space:]:' | sed 's/^04//')
+
+# Get the private key
 priv=$(echo "$key" | grep priv -A 3 | tail -n +2 | tr -d '\n[:space:]:' | sed 's/^0//')
+
+# Derive the address from the last 160 less significant bytes of the public key
 address=$(echo "$pub" | ./keccak-256sum -x -l | tr -d ' -' | tail -c 41)
 
-
 # Results
+clear
 echo "Pair of keys:
 
 $key
@@ -19,4 +25,5 @@ Private key:
 $priv
 
 Address:
-0x$address"
+0x$address
+"
